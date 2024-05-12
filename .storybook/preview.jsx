acc7be0e87fbee58e348a4180c039889899f6d1f
nzkks <kkshanthosh@gmail.com>
2024-05-13 11:39:07 +1200

    Install my own storybook design system and use the same global styles defined in it

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,18 @@
+import { Global } from '@emotion/react';
+import { global as designSystemGlobal } from '@nzkks/storybook-design-system';
+
+const { GlobalStyle } = designSystemGlobal;
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    Story => (
+      <>
+        <Global styles={GlobalStyle} />
+        <Story />
+      </>
+    )
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
